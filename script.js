2a98ccb4df4fb5a7b0aaeba710aa28288b0eb6ba
gabyelizabet <126698477+gabyelizabet@users.noreply.github.com>
2023-03-29 13:00:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const button= document.getElementById("button");
-button.addEventListener('click', function mostrar_ocultar(lista_datos){
-    let lista_datos= document.getElementsByClassName("lista_datos");
-    let currentDisplay = lista_datos.style.display;
+let button= document.getElementById("button");
 
-}
-)
+button.addEventListener('click', function mostrar_ocultar())
+    
+let lista_datos = document.getElementsByClassName("lista_datos");
+
 function mostrar_ocultar(info){
     if (currentDisplay === "none"){
         info.style.display="flex";
